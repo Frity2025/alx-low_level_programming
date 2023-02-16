71ed87c1df59f17ef9cdb35e0736afc5e226d637
Frity2025 <frity362@gmail.com>
2023-02-16 15:06:45 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-
-    return (1);
+	const char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fputs(buffer, stdout);
+	return (1);
 }
