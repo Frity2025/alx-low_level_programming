6ede2e5cbf06908e221fdaf3bde6cdaed3803d3f
Frity2025 <frity362@gmail.com>
2023-02-16 12:38:41 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Size of a char:%lu byte(s)\n ",sizeof(char));
+    printf("Size of a int:%lu byte(s)\n ",sizeof(int));
+    printf("Size of a long int:%lu byte(s)\n",sizeof(long int));
+    printf("Size of a long long int :%lu byte(s)\n",sizeof(long long int));
+    return (0);
+
+}
