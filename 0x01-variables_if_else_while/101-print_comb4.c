d241db59991fe930d80aeb54fff561af3373452c
Frity2025 <frity362@gmail.com>
2023-02-17 14:50:49 -0800

    Task 11 updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ int x, y, z;
 
 for (x = 0; x < 8; x++)
 {
-for (y = y + 1; y < 9; y++)
+for (y = x + 1; y < 9; y++)
 {
 for (z = y + 1; z < 10; z++)
 {
-putchar((z % 10) + '0');
-putchar((z % 10) + '0');
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
 putchar((z % 10) + '0');
 
 if (x == 7 && y == 8 && z == 9)
