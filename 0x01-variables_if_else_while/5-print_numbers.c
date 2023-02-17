587514226d870fe50dae513c530a5beb08ca5077
Frity2025 <frity362@gmail.com>
2023-02-17 13:42:39 -0800

    Fifth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers
+ *
+ * Description: using the main function
+ * this program that prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+int j;
+
+for (j = 0; j < 10; j++)
+{
+printf("%i", j);
+}
+printf("\n");
+return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-a program that prints the alphabet in lowercase
+a program that prints all single digit numbers of base 10 starting from 0
