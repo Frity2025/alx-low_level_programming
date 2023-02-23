4f56759e947d65292c43a4cb30e8c0b4df63fd22
Frity2025 <frity362@gmail.com>
2023-02-23 14:42:03 -0800

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - print number between 0 to 9 except 2 and 4
- * Description: using the main function
- * this function print 0,1,3,5,6,7,8,9.
- * Return: 0
+ * print_numbers - function
+ *
+ * Description: Prints the numbers
+ *
+ * Return: nothing
  */
 void print_numbers(void)
 {
-	int n;
+	int c;
 
-	for (n = 0; n < 10; n++)
+	for (c = 48; c < 58; c++)
 	{
-		if (n != 2 && n != 4)
-			_putchar((n % 10) + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
