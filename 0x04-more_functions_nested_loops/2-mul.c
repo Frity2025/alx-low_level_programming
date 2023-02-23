59b11c75dc06e6adf9a0486082f50f7a14fc8e15
Frity2025 <frity362@gmail.com>
2023-02-23 13:50:37 -0800

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function that  print multiplication of two numbers
+ * @a:int a
+ * @b:int b
+ * Description: using the main function
+ * this function print the result of multipliaction of two numbers
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Function that checks for a digit (0 through 9)
+a function that multiplies two integers
