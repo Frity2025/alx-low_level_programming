6e4d01be381e42e149875b3d7e6792648ee0dee9
Frity2025 <frity362@gmail.com>
2023-02-27 16:15:48 -0800

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n)
-	{
-		printf("%d", a[i]);
-
-		if (i < n - 1)
-			printf(", ");
-	}
-	       i++;
+for (i = 0; i < n; i++)
+{
+	printf("%d,  ", a[i]);
 }
 printf("\n");
 }
