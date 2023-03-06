7947e7e01f9d5a9469472e3ab4b57a3649d8ec8a
Frity2025 <frity362@gmail.com>
2023-03-06 15:54:53 -0800

    Task 7 summaion of array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of the two diagonals arrays.
+ * @a:2d array
+ * @size:size of array
+ *
+ *
+ * Return: nothing.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i,  sumDr = 0, sumDl = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sumDr = sumDr + a[i];
+		sumDl = sumDl + a[size - i - 1];
+		a = a + size;
+	}
+	printf("%d , %d\n ", sumDr, sumDl);
+}
