2977755fa9e3e58a8f5771b387f15982e7171fe9
Frity2025 <frity362@gmail.com>
2023-03-06 11:35:25 -0800

    task 2 updated wit the code fail

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0' && c != '\0')
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 
 		++s;
 	}
+	if (c == '\0')
+		return (s);
 	return (0);
 }
