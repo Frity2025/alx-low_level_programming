43d52d4a2e22f459d8d17ce26f1ff062601cb45d
Frity2025 <frity362@gmail.com>
2023-03-16 04:35:52 -0700

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat  - check the code
+ * @s1:first string 
+ * @s2:second string
+ * @n:
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+
+	unsigned int i, j, k, l;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	if (n > j)
+		n = j;
+	k = i + n;
+	s3 = malloc(k +1);
+
+	if(s3 == NULL)
+		return (NULL);
+	for (l = 0; l < k; l++)
+		if (l < i)
+			s3[l] = s1[l];
+		else
+			s3[l] = s2[l -i];
+	s3[l] = '\0';
+	return (s3);
+}	
+
+
