09faa9809018b476aea3ddbe10414b7d8d01096d
Frity2025 <frity362@gmail.com>
2023-03-21 10:25:17 -0700

    Task updated

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,6 +4,7 @@
 
 /**
  * init_dog - initialize the variable
+ * @d:initalize the dog
  * @name:name of the dog
  * @age:age of the dog
  * @owner: owner of the dog
@@ -13,7 +14,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
