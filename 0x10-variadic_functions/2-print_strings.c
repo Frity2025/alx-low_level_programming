1a36e65974a6be06f18513712e64647cdcde2278
Frity2025 <frity362@gmail.com>
2023-03-23 12:40:23 -0700

    Task 2 updated

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (ch)
 			printf("%s", ch);
 		else
-			printf("(inil)");
+			printf("(nil)");
 		if (separator && i < n - 1)
 		{
 			printf("%s", separator);
