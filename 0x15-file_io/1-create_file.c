08f8eb38b9bb48f761ebd9e9d0b1a63a8d3b7d4a
Frity2025 <frity362@gmail.com>
2023-04-10 15:01:15 -0700

    Task 1 updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
 		count = 0;
 		while (text_content[count])
@@ -27,9 +27,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(file, text_content, count);
-	if (file == (-1))
-		return (-1);
-	if (w == (-1))
+	if (file == -1 || w == -1)
 		return (-1);
 	close(file);
 	return (1);
